c8d91e7db3d101250dbf141afb0f2b8967ab36bd
saverio <saverio@ef3c9aa7-e928-0410-9a28-8491e62b674b>
2009-11-13 14:27:42 +0000

    Tool to upload all your public keys to a key server (hardcoded keyserver becayse I am lazy)
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/misc_tools@737 ef3c9aa7-e928-0410-9a28-8491e62b674b

---
+++ upload-pgp-keys.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+import os
+from subprocess import Popen, PIPE, STDOUT
+
+cmd = "gpg --list-keys | grep pub |grep -v gnupg | cut -c 13-20"
+p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+
+tmp = p.stdout.readline()
+while tmp:
+	os.system("gpg --keyserver pgp.mit.edu --send-key " + tmp)
+	tmp = p.stdout.readline()
+
+
+
