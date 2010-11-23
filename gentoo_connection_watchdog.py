1ce88f4d888d6bd476b77989ffa3d2ac4ce31558
saverio <saverio@ef3c9aa7-e928-0410-9a28-8491e62b674b>
2010-11-23 09:46:58 +0000

    Adding tool to restart ppp0 if connection is down
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/misc_tools@866 ef3c9aa7-e928-0410-9a28-8491e62b674b

---
+++ gentoo_connection_watchdog.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import time
+import smtplib
+
+#implement watchdog for IPv6 that just restarts aiccu
+
+if (os.system("ping -c 1 www.google.com") !=0):
+	os.system("/etc/init.d/net.ppp0 restart")
+	time.sleep(10)
+	#restore IPv6
+	os.system("killall -9 aiccu")
+	print "start aiccu\n"
+	os.system("/usr/sbin/aiccu start")
+	server = smtplib.SMTP('localhost')
+	server.set_debuglevel(0)
+	server.sendmail("tuscoloserver@ninux.org", "zioproto@ninux.org", "Server al Tuscolo senza connessione, restaring")
+	server.quit()
+
