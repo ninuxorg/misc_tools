0bfe284427b1143c80a24d7a74e606bd00f10da2
LuX <LuX@ef3c9aa7-e928-0410-9a28-8491e62b674b>
2012-05-04 12:06:26 +0000

    aggiornato snmp-traceroute.sh e aggiunto loop_hunter.sh
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/misc_tools@1001 ef3c9aa7-e928-0410-9a28-8491e62b674b

--- loop_hunter.sh
+++ loop_hunter.sh
@@ -2,9 +2,10 @@
 EXPECTED_ARGS=1 # Number of arguments expected.
 E_BADARGS=85    # Exit value if incorrect number of args passed.
 test $# -ne ${EXPECTED_ARGS} && echo "Usage: `basename $0` IPv4" && exit ${E_BADARGS}
-ElencoHop=$( traceroute -n ${1} 2>/dev/null | tail -n +2 | awk '{print $2}' | sed 's/*//g' )
+echo "It may take time. Please wait.."
+ElencoHopDuplicati=$( traceroute -n ${1} 2>/dev/null | tail -n +2 | awk '{print $2}' | sed 's/*//g' | sort -n | uniq -d )
 echo "IPv4      HostName        Contact"
-for IPv4 in ${ElencoHop}; do
+for IPv4 in ${ElencoHopDuplicati}; do
         NomeHost=$( snmpwalk -v 1 -c public ${IPv4} 1.3.6.1.2.1.1.5.0 2>/dev/null | awk '{print $4}' )
         Contatto=$( snmpwalk -v 1 -c public ${IPv4} 1.3.6.1.2.1.1.4.0 2>/dev/null | awk '{print $4}' )
         echo "${IPv4}   ${NomeHost}     ${Contatto}"
