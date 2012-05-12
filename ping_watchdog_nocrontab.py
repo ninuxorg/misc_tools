c32bf411431a51bc9c339a28ed8cd4886774924c
saverio <saverio@ef3c9aa7-e928-0410-9a28-8491e62b674b>
2012-05-12 19:58:29 +0000

    Implemented IPv6 ping watchdog indipendent from crontab
    Implemented better logging
    
    Added -r flag to ping6 to make ping independent from routing table
    
    reworked to have all variables set at the top of the file
    
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/misc_tools@1008 ef3c9aa7-e928-0410-9a28-8491e62b674b

---
+++ ping_watchdog_nocrontab.py
@@ -0,0 +1,34 @@
+import os 
+import sys
+import time
+import smtplib
+import datetime
+
+IPtocheck = ""
+smtpserver = ""
+fromemail = ""
+toemail = ""
+subject = ""
+body = ""
+logfile = ""
+
+def logToFile(string,logfile):
+	f=open(logfile,'a')
+	f.write(str(datetime.datetime.now())+string+"\n")
+	f.close()
+
+
+while True:
+	if (os.system("ping6 -c 1 -r "+IPtocheck+" > /dev/null") !=0):
+		if (os.system("ping6 -c 1 -r "+IPtocheck+" > /dev/null") !=0):
+			if (os.system("ping6 -c 1 -r "+IPtocheck+" > /dev/null") !=0):
+			        server = smtplib.SMTP(smtpserver)
+	        		server.set_debuglevel(0)
+				msg = "From:"+fromemail+"\r\nTo:"+toemail+"\r\nSubject:"+subject
+	        		logToFile(body,logfile)
+				server.sendmail(fromemail, toemail, msg)
+	        		server.quit()
+				time.sleep(30)
+	else:
+		time.sleep(2)
+
