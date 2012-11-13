bc5bfa2f25febb2e23fc93589aa34c9f6134dd28
LuX <LuX@ef3c9aa7-e928-0410-9a28-8491e62b674b>
2012-11-13 22:09:56 +0000

    snmp-traceroute.sh Updated: Now shows the output during the traceroute
    git-svn-id: https://svn.ninux.org/svn/ninuxdeveloping/misc_tools@1044 ef3c9aa7-e928-0410-9a28-8491e62b674b

--- snmp-traceroute.sh
+++ snmp-traceroute.sh
@@ -1,12 +1,32 @@
-#!/bin/bash
+#!/bin/sh
+
 EXPECTED_ARGS=1 # Number of arguments expected.
 E_BADARGS=85    # Exit value if incorrect number of args passed.
 test $# -ne ${EXPECTED_ARGS} && echo "Usage: `basename $0` IPv4" && exit ${E_BADARGS}
-ElencoHop=$( traceroute -n ${1} 2>/dev/null | tail -n +2 | awk '{print $2}' | sed 's/*//g' )
-echo "IPv4      HostName        Contact"
-for IPv4 in ${ElencoHop}; do
-        NomeHost=$( snmpwalk -v 1 -c public ${IPv4} 1.3.6.1.2.1.1.5.0 2>/dev/null | awk '{print $4}' )
-        Contatto=$( snmpwalk -v 1 -c public ${IPv4} 1.3.6.1.2.1.1.4.0 2>/dev/null | awk '{print $4}' )
-        echo "${IPv4}   ${NomeHost}     ${Contatto}"
+
+# Track dependencies
+E_BADEPENDS=90  # Exit value if unmet dependencies.
+ErrorMsg=""
+SnmpWalk=$(which snmpwalk)    	|| ErrorMsg="snmpwalk";
+Tail=$(which tail)            	|| ErrorMsg="tail";
+Awk=$(which awk)              	|| ErrorMsg="awk";
+Sed=$(which sed)              	|| ErrorMsg="sed";
+Echo=$(which echo)            	|| ErrorMsg="echo";
+TraceRoute=$(which traceroute)	|| ErrorMsg="traceroute";
+
+if [ ! -z "${ErrorMsg}" ] ; then
+	echo "Command not found: ${ErrorMsg}"
+	exit ${E_BADEPENDS}
+fi
+
+${Echo} "IPv4			HostName			Contact"
+
+${TraceRoute} -n ${1} 2>/dev/null | while read TraceRow; do 
+	IPv4=$( ${Echo} "${TraceRow}" | ${Awk} '{print $2}' | ${Sed} 's/*//g' )
+	${Echo} -n "${IPv4}		"
+	HostName=$( ${SnmpWalk} -v 1 -c public ${IPv4} 1.3.6.1.2.1.1.5.0 2>/dev/null | ${Awk} '{print $4}' )
+	Contact=$( ${SnmpWalk} -v 1 -c public ${IPv4} 1.3.6.1.2.1.1.4.0 2>/dev/null | ${Awk} '{print $4}' )
+	${Echo} "${HostName}			${Contact}" 
 done
+
 exit 0
