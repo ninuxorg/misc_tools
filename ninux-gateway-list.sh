49944b6a44c9a301673f0ce61065b072ca8f39bb
Saverio Proto <zioproto@gmail.com>
2013-06-11 09:25:42 +0200

    Passate il nodo OLSR da interrogare come primo argomento

--- ninux-gateway-list.sh
+++ ninux-gateway-list.sh
@@ -1,3 +1,7 @@
-wget -q -O - http://IP_DELLA_MACCHINA_OLSR:2006/hna | grep "/0" | sort -n | awk '{print $2}' | while read ip_add; do snmpwalk -v1 -c public ${ip_add} iso.3.6.1.2.1.1.5.0 | awk '{print $4}' | while read ip_name; do echo -e "${ip_add} ${ip_name}"; done; done
+#!/bin/bash
+
+#usage: ./ninux-gateway-list.sh ipaddressofolsrnodewithtxtinfo
+
+wget -q -O - http://$1:2006/hna | grep "/0" | sort -n | awk '{print $2}' | while read ip_add; do snmpwalk -v1 -c public ${ip_add} iso.3.6.1.2.1.1.5.0 | awk '{print $4}' | while read ip_name; do echo -e "${ip_add} ${ip_name}"; done; done
 
 
