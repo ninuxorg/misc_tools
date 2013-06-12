b41f2cf51cb8d0c4389c52178c1d8a69e4337b83
arkanet <arkantiko@gmail.com>
2013-06-12 13:16:01 +0200

    removed "-e" option to echo command

--- ninux-gateway-list.sh
+++ ninux-gateway-list.sh
@@ -2,6 +2,6 @@
 
 #usage: ./ninux-gateway-list.sh ipaddressofolsrnodewithtxtinfo
 
-wget -q -O - http://$1:2006/hna | grep "/0" | sort -n | awk '{print $2}' | while read ip_add; do snmpwalk -v1 -c public ${ip_add} iso.3.6.1.2.1.1.5.0 | awk '{print $4}' | while read ip_name; do echo -e "${ip_add} ${ip_name}"; done; done
+wget -q -O - http://$1:2006/hna | grep "/0" | sort -n | awk '{print $2}' | while read ip_add; do snmpwalk -v1 -c public ${ip_add} iso.3.6.1.2.1.1.5.0 | awk '{print $4}' | while read ip_name; do echo "${ip_add} ${ip_name}"; done; done
 
 
