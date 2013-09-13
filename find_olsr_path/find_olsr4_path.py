e5d58b73094f5ed7e2a7dadac75ae0c22ecf9190
Claudio Pisa <clauz@ninux.org>
2013-09-13 14:04:52 +0200

    find_olsr4_path: add author

--- find_olsr_path/find_olsr4_path.py
+++ find_olsr_path/find_olsr4_path.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python2
 # vim:ts=2:expandtab
+#
+#  Copyright 2013 Claudio Pisa (clauz at ninux dot org)
+#
+#  This file is part of find_olsr_path
+#
+#  find_olsr_path is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  find_olsr_path is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with find_olsr_path.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 
 import networkx as nx
 import urllib2
